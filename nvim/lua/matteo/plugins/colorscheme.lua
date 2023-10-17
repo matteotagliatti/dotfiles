21ddd43c1e4ecc36e7b6d20358944b0f59cfa496
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-10-17 18:27:34 +0200

    tmux and nvim catpuccin fix

--- nvim/lua/matteo/plugins/colorscheme.lua
+++ nvim/lua/matteo/plugins/colorscheme.lua
@@ -3,6 +3,14 @@ return {{
     name = "catppuccin",
     priority = 1000, -- to load this before all the other plugins
     config = function()
+        local catppuccin = require("catppuccin")
+
+        catppuccin.setup({
+            flavour = "mocha"
+        })
+
+        -- load the colorscheme here
+        vim.o.termguicolors = true
         vim.cmd.colorscheme "catppuccin"
     end
 }}

5925fa46b62d9253ee50dd9900c0174539ff9b89
Matteo Tagliatti <matteotagliatti@gmail.com>
2024-03-27 09:41:08 +0100

    nvim keymaps

--- .config/nvim/lua/matteo/core/keymaps.lua
+++ .config/nvim/lua/matteo/core/keymaps.lua
@@ -27,10 +27,7 @@ keymap.set("n", "<leader>dq", vim.diagnostic.setloclist, { desc = "Open [D]iagno
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-
--- Today date in YYYY-MM-DD format
-keymap.set("n", "<leader>dt", "i<C-R>=strftime('%Y-%m-%d')<CR>", { desc = "Insert today's date" })
+keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })

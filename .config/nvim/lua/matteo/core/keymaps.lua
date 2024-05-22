24890c7a3e9943687138d5b943e82f5e8b12cbd2
Matteo Tagliatti <matteotagliatti@gmail.com>
2024-05-22 18:32:16 +0200

    add nvim keymaps

--- .config/nvim/lua/matteo/core/keymaps.lua
+++ .config/nvim/lua/matteo/core/keymaps.lua
@@ -15,9 +15,17 @@ keymap.set("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move line up" })
 -- buffers
 keymap.set("n", "<tab>", ":bnext<Return>", { desc = "Go to next buffer" })
 keymap.set("n", "<s-tab>", ":bprev<Return>", { desc = "Go to previous buffer" })
-keymap.set("n", "<leader>q", ":bdelete<Return>", { desc = "Delete buffer" })
+keymap.set("n", "<leader>qq", ":bdelete<Return>", { desc = "Delete buffer" })
 -- keymap.set("n", "8", ":bfirst<enter>", { desc = "Go to last buffer" })
 -- keymap.set("n", "9", ":blast<enter>", { desc = "Go to first buffer" })
+keymap.set("n", "<leader>qa", function() -- Close all buffers except the current one
+	local current_bufnr = vim.fn.bufnr("%")
+	for _, bufnr in ipairs(vim.fn.getbufinfo({ buflisted = 1 })) do
+		if bufnr.bufnr ~= current_bufnr then
+			vim.api.nvim_buf_delete(bufnr.bufnr, { force = true })
+		end
+	end
+end, { desc = "Close all buffers except the current one" })
 
 -- Diagnostic keymaps
 keymap.set("n", "<leader>dn", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
@@ -32,3 +40,17 @@ keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" 
 keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- Daily Notes
+-- inside ./daily directory
+keymap.set("n", "<leader>nt", function()
+	local date = os.date("%Y-%m-%d")
+	local filename = string.format("%s/%s.md", vim.fn.expand("./daily"), date)
+	vim.cmd("e " .. filename)
+end, { desc = "Create a new daily note" })
+
+keymap.set("n", "<leader>ny", function()
+	local date = os.date("%Y-%m-%d", os.time() - 86400)
+	local filename = string.format("%s/%s.md", vim.fn.expand("./daily"), date)
+	vim.cmd("e " .. filename)
+end, { desc = "Create a new daily note for yesterday" })

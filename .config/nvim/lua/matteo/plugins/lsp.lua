35715c0c9bb21ca4a21e8c1933a36b8999c66a5d
Matteo Tagliatti <matteotagliatti@gmail.com>
2024-07-22 16:42:30 +0200

    re add react in emmet filetypes

--- .config/nvim/lua/matteo/plugins/lsp.lua
+++ .config/nvim/lua/matteo/plugins/lsp.lua
@@ -64,7 +64,7 @@ return { -- LSP Configuration & Plugins
 			cssls = {},
 			tailwindcss = {},
 			emmet_ls = {
-				filetypes = { "html", "css", "astro" },
+				filetypes = { "html", "css", "astro", "javascriptreact" },
 			},
 			astro = {},
 			lua_ls = {

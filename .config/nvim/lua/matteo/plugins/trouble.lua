4aa6fcd22ed697b25ed1c418d225ffb5e9141943
Matteo Tagliatti <matteotagliatti@gmail.com>
2024-09-16 09:12:58 +0200

    nvim better trouble setup

--- .config/nvim/lua/matteo/plugins/trouble.lua
+++ .config/nvim/lua/matteo/plugins/trouble.lua
@@ -5,12 +5,12 @@ return {
 		local t = require("trouble")
 
 		vim.keymap.set("n", "<leader>tt", function()
-			t.toggle("telescope_files")
+			t.toggle("telescope")
 		end, { desc = "[T]rouble [T]elescope" })
 
-		vim.keymap.set("n", "<leader>ta", function()
-			t.toggle("telescope")
-		end, { desc = "[T]rouble Telescope [A]ll" })
+		vim.keymap.set("n", "<leader>tf", function()
+			t.toggle("telescope_files")
+		end, { desc = "[T]rouble Telescope [F]iles" })
 
 		vim.keymap.set("n", "<leader>td", function()
 			t.toggle("diagnostics")
@@ -18,6 +18,6 @@ return {
 
 		vim.keymap.set("n", "<leader>tq", function()
 			t.toggle("quickfix")
-		end, { desc = "[T]trouble Quickfix" })
+		end, { desc = "[T]rouble [Q]uickfix" })
 	end,
 }

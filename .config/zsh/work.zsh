ed5f96c0beaa32e2f451bff36ad619d3b2e7b822
Matteo Tagliatti <matteotagliatti@gmail.com>
2025-04-06 01:48:41 +0200

    small stuff

--- .config/zsh/work.zsh
+++ .config/zsh/work.zsh
@@ -1,5 +1,4 @@
 # work aliases
 alias dr="sudo sysctl -w kernel.apparmor_restrict_unprivileged_userns=0 && systemctl --user restart docker-desktop"
 alias gl="gulp 'launch services'"
-alias gi="gulp 'install services'"
 alias filezilla="$HOME/Applications/FileZilla_3.67.1_x86_64-linux-gnu/FileZilla3/bin/filezilla"

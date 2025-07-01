1a8c9f528aae10da13586f218b6de48a1350a947
Matteo Tagliatti <matteotagliatti@gmail.com>
2025-07-01 17:29:52 +0200

    npm aliases

--- .config/zsh/work.zsh
+++ .config/zsh/work.zsh
@@ -2,4 +2,6 @@ alias gl="gulp 'launch services'"
 
 alias nss="npm run start:services"
 alias ns="npm run start -- debug"
-alias nl="npm run link"
+alias nl="npm i && npm run link"
+alias nw="npm run watch"
+alias nd="npm run dev"

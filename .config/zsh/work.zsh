4e860234108d3c3dfe16c625b155453ee73523f5
Matteo Tagliatti <matteotagliatti@gmail.com>
2025-07-09 09:41:57 +0200

    small fix to work zsh

--- .config/zsh/work.zsh
+++ .config/zsh/work.zsh
@@ -1,7 +1,7 @@
 alias gl="gulp 'launch services'"
 
 alias nss="npm run start:services"
-alias ns="npm run start -- debug"
+alias ns="npm run start -- --debug"
 alias nl="npm i && npm run link"
 alias nw="npm run watch"
 alias nd="npm run dev"

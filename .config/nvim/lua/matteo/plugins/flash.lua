c7f7a7f34ca63da78cc9b72e12a0a257c7c1a07c
Matteo Tagliatti <matteotagliatti@gmail.com>
2025-07-27 00:01:37 +0200

    nvim change flash keymap and add mini.surround

--- .config/nvim/lua/matteo/plugins/flash.lua
+++ .config/nvim/lua/matteo/plugins/flash.lua
@@ -4,8 +4,8 @@ return {
 	opts = {},
   -- stylua: ignore
   keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "[F]lash" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "[F]lash Treesitter" },
+    { "<leader>s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "[F]lash" },
+    { "<leader>S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "[F]lash Treesitter" },
     { "r", mode = "o", function() require("flash").remote() end, desc = "[R]emote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "[R]emote Treesitter Search" },
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },

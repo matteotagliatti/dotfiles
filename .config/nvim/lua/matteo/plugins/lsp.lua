21e3eebeff9e1ee302a32bf94a9b34874b252e92
Matteo Tagliatti <matteotagliatti@gmail.com>
2025-07-29 11:33:19 +0200

    better nvim lsp keybind
    update

--- .config/nvim/lua/matteo/plugins/lsp.lua
+++ .config/nvim/lua/matteo/plugins/lsp.lua
@@ -10,13 +10,25 @@ return { -- LSP Configuration & Plugins
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
 			callback = function(event)
-				local map = function(keys, func, desc)
-					vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+				local map = function(keys, func, desc, mode)
+					mode = mode or "n"
+					vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 				end
+
+				-- Rename the variable under your cursor.
+				--  Most Language Servers support renaming across files, etc.
+				map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
+
+				-- Execute a code action, usually your cursor needs to be on top of an error
+				-- or a suggestion from your LSP for this to activate.
+				map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
+
+				-- Find references for the word under your cursor.
+				map("grr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
 
 				-- Jump to the implementation of the word under your cursor.
 				--  Useful when your language has ways of declaring types without an actual implementation.
-				map("gi", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+				map("gri", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
 
 				-- Jump to the definition of the word under your cursor.
 				--  This is where a variable was first declared, or where a function is defined, etc.
@@ -25,7 +37,7 @@ return { -- LSP Configuration & Plugins
 
 				-- WARN: This is not Goto Definition, this is Goto Declaration.
 				--  For example, in C this would take you to the header.
-				map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+				map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
 				-- Fuzzy find all the symbols in your current document.
 				--  Symbols are things like variables, functions, types, etc.
@@ -38,7 +50,7 @@ return { -- LSP Configuration & Plugins
 				-- Jump to the type of the word under your cursor.
 				--  Useful when you're not sure what type a variable is and you want to see
 				--  the definition of its *type*, not where it was *defined*.
-				map("gt", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype Definition")
+				map("grt", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype Definition")
 			end,
 		})
 

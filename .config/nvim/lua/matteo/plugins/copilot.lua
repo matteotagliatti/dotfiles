045f27bdef74574ab6f29c9554bc4e6428168577
Matteo Tagliatti <matteotagliatti@gmail.com>
2026-02-11 16:06:32 +0100

    update git conf, copilot.lua and alacritty opacity

--- .config/nvim/lua/matteo/plugins/copilot.lua
+++ .config/nvim/lua/matteo/plugins/copilot.lua
@@ -1,21 +1,21 @@
 return {
-	"zbirenbaum/copilot.lua",
-	cmd = "Copilot",
-	build = ":Copilot auth",
-	event = "BufReadPost",
-	opts = {
-		suggestion = {
-			auto_trigger = true,
-			keymap = {
-				accept = "<Tab>",
-				next = "<C-]>",
-				prev = "<C-[>",
-			},
-		},
-		panel = { enabled = false },
-		filetypes = {
-			markdown = true,
-			help = true,
-		},
-	},
+	-- "zbirenbaum/copilot.lua",
+	-- cmd = "Copilot",
+	-- build = ":Copilot auth",
+	-- event = "BufReadPost",
+	-- opts = {
+	-- 	suggestion = {
+	-- 		auto_trigger = true,
+	-- 		keymap = {
+	-- 			accept = "<Tab>",
+	-- 			next = "<C-]>",
+	-- 			prev = "<C-[>",
+	-- 		},
+	-- 	},
+	-- 	panel = { enabled = false },
+	-- 	filetypes = {
+	-- 		markdown = true,
+	-- 		help = true,
+	-- 	},
+	-- },
 }

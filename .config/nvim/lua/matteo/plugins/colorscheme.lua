3d77f9bda2f78101016fd8d25a6eda05f8261ea3
Matteo Tagliatti <matteotagliatti@gmail.com>
2026-03-25 21:12:51 +0100

    simpler conifg

--- .config/nvim/lua/matteo/plugins/colorscheme.lua
+++ .config/nvim/lua/matteo/plugins/colorscheme.lua
@@ -1,16 +1,16 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000, -- to load this before all the other plugins
-    config = function()
-        local catppuccin = require("catppuccin")
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000, -- to load this before all the other plugins
+	config = function()
+		local catppuccin = require("catppuccin")
 
-        catppuccin.setup({
-            flavour = "mocha"
-        })
+		catppuccin.setup({
+			flavour = "mocha",
+		})
 
-        -- load the colorscheme here
-        vim.o.termguicolors = true
-        vim.cmd.colorscheme "catppuccin"
-    end
+		-- load the colorscheme here
+		vim.o.termguicolors = true
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
---
+++ .config/nvim/lua/matteo/plugins/notes.lua
@@ -0,0 +1,4 @@
+return {
+	"matteotagliatti/notes.nvim",
+	opts = {},
+}
